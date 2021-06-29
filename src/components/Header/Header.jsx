9ac7ed3780519afad15fe5e118e3f10e09434f6b
Murtaja <murtajaadnan7@gmail.com>
2021-06-29 20:06:26 +0300

    Feat: create the header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Typography } from "@material-ui/core"
+
+function Header() {
+	return (
+		<div>
+			<Typography variant="h3">Card Game</Typography>
+		</div>
+	)
+}
+
+export default Header
---
+++ src/containers/Container.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "../components/Header/Header"
+
+function Container() {
+	return (
+		<div>
+			<Header />
+		</div>
+	)
+}
+
+export default Container
